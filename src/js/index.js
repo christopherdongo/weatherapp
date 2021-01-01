0e6aa8317574c0c77ca9c2a13d652b46cff029c3
darkzero <darkzero@localhost.localdomain>
2021-01-01 04:25:27 -0500

    commit 2

--- src/js/index.js
+++ src/js/index.js
@@ -14,46 +14,73 @@
   const icono = document.getElementById('icon');
   const temperatura = document.getElementById('temp');
   const loader = document.getElementById('loader');
+  //const secondary = document.getElementById('secondary');
 
   //escucha del formulario
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  form.addEventListener("submit", (e)=>{
+    obtainsData(e);
+  } );
+  form.addEventListener("keyup", (e)=>{
+     if(e.key=="Enter"){
+       obtainsData(e);
+     }
+  })
 
+  const obtainsData=(e)=>{
+    e.preventDefault();
+    Clear();
     //fetch de los datos
     const promise1 = fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=${weather.APIKEY}&lang=es`
+      `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=${weather.APIKEY}&lang=es`
     );
     const promise2 = fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${search.value}&appid=${weather.APIKEY}&lang=es`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${search.value}&units=metric&appid=${weather.APIKEY}&lang=es`
     );
     //resolver la promesa
     Promise.all([promise1, promise2]).then( response => {return Promise.all(response.map( r => r.json())) })
     .then( ([data, nextdata]) => { ObtenerDatos(data), OBtenerDatosNext(nextdata)}).catch( e => { ObtenerError(e)})
+    
+  }
 
-  });
   //function primera tarheta
   const ObtenerDatos = (data) => {
-
     const {name} = data;
     const {icon, description} = data.weather[0];
     const {humidity, temp} = data.main; 
     const {speed} = data.wind;
-    console.log(data)
-     title.innerText = "Clima de " + name;
-     temperatura.innerText =  ((temp - 273.15)*1.000000).toFixed(2) + " °C"
-     icono.src="https://openweathermap.org/img/wn/"+ icon +".png";
-     icono.alt= name;
-     descripcion.innerText = "Description: "+ description; 
-     humedad.innerText = ' Humedad:' + humidity + "%";
-     wind.textContent =" Velocidad del Viento:" + speed + "km/h"
+     console.log(data)
+      icono.src="https://openweathermap.org/img/wn/"+ icon +".png";
+      title.innerText = "Clima de " + name;
+      temperatura.innerText = temp + " °C"
+      icono.alt= name;
+      descripcion.innerText = description; 
+      humedad.innerText = ' Humedad: ' + humidity + " %";
+      wind.textContent =" Velocidad del Viento: " + speed + " km/h";
+      SpinnerViews('none')
   };
-  //function de los siguientes dias
+  //function de los dias siguientes!!
   const OBtenerDatosNext = (data) => {
-   console.log(data)
+    console.log(data)
   };
   //visualizar datos
-
   const ObtenerError = (message) => {
       console.log(message)
   };
+ //spinner
+  const SpinnerViews=(views)=>{
+          loader.style.display=views;
+  }
+
+  const Clear=()=>{
+    title.innerText = "";
+    temperatura.innerText ="";
+    icono.src="";
+    icono.alt="";
+    descripcion.innerText ="";
+    humedad.innerText = "";
+    wind.textContent ="";
+  }
+
+
+
 })();
