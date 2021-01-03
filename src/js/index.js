9b0a6c8350b360d196834ced773096c9f9166930
darkzero <darkzero@localhost.localdomain>
2021-01-03 02:34:06 -0500

    ajustes del DOM

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@
   };
 
   //constantes
+  const firstrow= document.getElementById('first-row');
   const form = document.getElementById("formulario");
   const search = document.getElementById("inputciudad");
   const title = document.getElementById("title");
@@ -16,19 +17,15 @@
   const loader = document.getElementById("loader");
   const error = document.getElementById("error");
   //const secondary = document.getElementById('secondary');
-
   //creacion de elementos para las siguientes tarjetas
-  const thrid = document.querySelector('#thrid');
+  //const thrid = document.querySelector('#thrid');
+  let  thrid2 = document.createElement('section');
+        thrid2.id="thrid2";
+        thrid2.classList="card card--thrid"
+  let thridremove = document.querySelector('#thrid2'); 
 
-  let h2title = document.createElement("h2");
-  let cardContainer2 = document.createElement("div");
-  let divfecha = document.createElement('div');
-  let divhumedad = document.createElement("div");
-  let divwind = document.createElement("div");
-  let imgicono = document.createElement("img");
-  let divdescription2 = document.createElement('div');
-  let divtemperatura = document.createElement("div");
-  let cardnext = document.createElement('article');
+  //segunda tarjeta
+
   //seleccionar la tarjeta creada
 
   //escucha del formulario
@@ -43,12 +40,22 @@
 
   const obtainsData = (e) => {
     e.preventDefault();
+
+    if(firstrow.children.length==3){
+      console.log('existe')
+      firstrow.parentNode.removeChild(thridremove)
+    }else{
+      console.log('no existe')
+    }
+   
+      firstrow.appendChild(thrid2)
+    
     //fetch de los datos
     const promise1 = fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=${weather.APIKEY}&lang=es`
     );
     const promise2 = fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${search.value}&units=metric&appid=${weather.APIKEY}&lang=es`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${search.value}&units=metric&&appid=${weather.APIKEY}&lang=es`
     );
     //resolver la promesa
     Promise.all([promise1, promise2])
@@ -60,8 +67,9 @@
       })
       .catch((err) => console.log(err));
     SpinnerViews("block");
-    Clear();
     ObtenerError("none");
+    Clear();
+    
   };
 
   //function primera tarheta
@@ -84,7 +92,7 @@
       wind.textContent = " Velocidad del Viento: " + speed + " km/h";
       SpinnerViews("none");
       OBtenerDatosNext;
-      
+      firstrow.appendChild(thrid2)
     }
   };
   //visualizar datos
@@ -111,36 +119,80 @@
 
   //function de los dias siguientes!!
   const OBtenerDatosNext = (nextdata) => {
-    console.log(nextdata.list)
-    h2title.id="h2title";
-    cardContainer2.id="card__container2";
-    divtemperatura.id="divtemperature"
-    divhumedad.id="divhumedad";
-    divwind.id="divwind";
-    divdescription2.id="divdescription2";
-    imgicono.id="imgicono";
-    divfecha.id="divfecha";
-
-
-    thrid.appendChild(cardnext);
+   console.log(nextdata)
+    let result;
+    //thrid.appendChild(cardnext);
     //poner datos en el cardnext
-    cardnext.appendChild(h2title);
-    cardnext.appendChild(divfecha);
-    cardnext.appendChild(divtemperatura);
-    cardnext.appendChild(cardContainer2);
-    cardnext.appendChild(divhumedad);
-    cardnext.appendChild(divwind);
+    result = timesConvert(nextdata.list);
+    console.log(result)
+  
+    result.map( (item, index) => {
+      let newElement = document.createElement('article');
+      let cardContainer2 = document.createElement("div");
+      let divfecha = document.createElement('div');
+      let divhumedad = document.createElement("div");
+      let divwind = document.createElement("div");
+      let imgicono = document.createElement("img");
+      let divdescription2 = document.createElement('div');
+      let divtemperatura = document.createElement("div");
+      /*poner datos*/
+      cardContainer2.id="container2";
+      cardContainer2.className="card2__container2";
+      divtemperatura.id="divtemperature";
+      divtemperatura.className="card2__divtemperature";
+      divhumedad.id="divhumedad";
+      divhumedad.className="card__divhumedad"
+      divwind.id="divwind";
+      divdescription2.id="divdescription2";
+      imgicono.id="imgicono";
+      divfecha.id="divfecha";
+      divfecha.className="card2__divfecha" 
+      /*introducir la informacion*/
+      divfecha.textContent=item.data.dt_txt
+      divtemperatura.innerText = item.data.main.temp + " °C";
 
-    cardContainer2.appendChild(imgicono);
-    cardContainer2.appendChild(divdescription2);
-    
-
- 
+      newElement.id = index; newElement.className = "card2";
+      newElement.appendChild(divfecha);
+      newElement.appendChild(divtemperatura);
+      newElement.appendChild(cardContainer2);
+      newElement.appendChild(divhumedad);
+      newElement.appendChild(divwind);
+      cardContainer2.appendChild(imgicono);
+      cardContainer2.appendChild(divdescription2);
+         return(
+          thrid2.appendChild(newElement)
+         )
+    })
     
   };
 
 
-
+  /*resolucion de los date*/ 
+  const timesConvert=(data)=>{
+   return data.filter( item => (
+    convert(item.dt) === 6 || convert(item.dt) === 12 || convert(item.dt) === 18
+    )).map( item => (
+      {weekDay: convertDay(item.dt),
+        hour: convertHour(item.dt),
+        data:item
+      }
+    ) )
+  }
+  //conversion de datos
+  const convert=(dt)=>{
+    let t = new Date(dt * 1000).getUTCHours();
+    return t;
+  }
+  const convertDay=(dt)=>{
+    let d = new Date(dt*1000).getDate();
+    let m = new Date(dt*1000).getMonth()+1;
+    let a = new Date(dt*1000).getFullYear();
+    return `${d}/${m}/${a}`;
+  }
+  const convertHour=(dt)=>{
+    let t= new Date(dt*1000).getUTCHours();
+    return t;
+  }
 
 
 })();
